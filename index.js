60938f64ba5d5fe9a68af72b3fb261c2268099e1
fabriciojaime <fabricioojaime@gmail.com>
2020-05-27 18:01:29 -0300

    Atualizando

--- index.js
+++ index.js
@@ -1,39 +1,74 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');  //recebe dados de formulários
+
+// MÓDULOS ASTERISK
 const AsteriskManager = require('asterisk-manager');
 const ami = new AsteriskManager('5038','192.168.0.250','admin','123456',true);
 
-/*//ESCUTA TODOS OS EVENTOS
-ami.on('managerevent', function(evt) {
-   console.log(evt);
-});*/
+// ENGINE
+app.set('view engine','ejs');
+app.use(express.static("src"));
 
-/*ami.on('peerstatus', function({
+// BODY-PARSER
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+//RECEBE STATUS RAMAL
+var dados = ami.on('extensionstatus', function(event){ 
+    console.log(event)
+});
+
+// ROTAS
+app.get('/',(req,res)=>{
+    res.render('index',{
+        dados: dados
+    });  
+});
+
+//RECEBE TODOS OS EVENTOS
+/*
+ami.on('managerevent', function(evt) {
+    console.log(evt);
+});
+*/
+
+// VERIFICA RAMAL / STATUS / IP
+/*
+ami.on('peerstatus', function({
     peer,
     peerstatus,
     address
 }){ 
     console.log("RAMAL: " + peer +", STATUS: "+peerstatus+", IP: "+address);
-});*/
+});
+*/
 
-/*ami.action({
+// ENVIA COMANDO A CLI
+/*
+ami.action({
     'action':'command',
     'command':'sip show peers'
-  }, function(err, res) {
-      console.log(res)
-  });*/
+}, function(err, res) {
+    console.log(res)
+});
+*/
+
+//RECEBE STATUS RAMAL
+/* 
+ami.on('ExtensionStatus', function({event}){ 
+    console.log(event);
+});
+*/
 
 
-  // ExtensionStatus
-  // COM FILTRO
- /* ami.on('ExtensionStatus', function({
-    exten,
-    context,
-    status,
-    statustext    
-}){ 
-    console.log(exten,context,status,statusText);
-});*/
 
-// SEM FILTRO
-ami.on('ExtensionStatus', function(evt) {
-    console.log(evt);
+const portNumber = 80;
+app.listen(portNumber,(erro) => {
+    if(erro){
+        console.log("Não foi possível realizar a conexão com o servidor, erro: "+erro);
+    }else{
+        console.log("Servidor conectado na porta "+portNumber);
+    }
 });
