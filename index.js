6e833d8bb2696e200eb14d74aeced7c459bb7b41
fabriciojaime <fabricioojaime@gmail.com>
2020-05-27 15:26:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const AsteriskManager = require('asterisk-manager');
 const ami = new AsteriskManager('5038','192.168.0.250','admin','123456',true);
 
-//ESCUTA TODOS OS EVENTOS
-/*ami.on('managerevent', function(evt) {
+/*//ESCUTA TODOS OS EVENTOS
+ami.on('managerevent', function(evt) {
    console.log(evt);
 });*/
 
-ami.on('peerstatus', function({
+/*ami.on('peerstatus', function({
     peer,
     peerstatus,
     address
 }){ 
     console.log("RAMAL: " + peer +", STATUS: "+peerstatus+", IP: "+address);
-});
+});*/
 
-ami.action({
+/*ami.action({
     'action':'command',
     'command':'sip show peers'
   }, function(err, res) {
       console.log(res)
-  });
+  });*/
+
+
+  // ExtensionStatus
+  // COM FILTRO
+ /* ami.on('ExtensionStatus', function({
+    exten,
+    context,
+    status,
+    statustext    
+}){ 
+    console.log(exten,context,status,statusText);
+});*/
+
+// SEM FILTRO
+ami.on('ExtensionStatus', function(evt) {
+    console.log(evt);
+});
